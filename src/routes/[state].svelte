bd49584231c3a7759a21c70def22c48d80c191a9
diego enriquez-serrano <diego@provi.com>
2020-05-16 13:05:06 -0500

    merging updates to master

---
+++ src/routes/[state].svelte
@@ -0,0 +1,32 @@
+<script context="module">
+
+  export async function preload(page) {
+    console.log(page)
+    return {
+      state: page.params['state']
+    }
+  };
+
+</script>
+
+<script>
+
+  import CovidStat from '../components/CovidStat.svelte';
+  import CovidChart from '../components/CovidChart.svelte';
+  import TableContainer from '../components/TableContainer.svelte';
+  export let state;
+
+</script>
+
+<svelte:head>
+  <title>Covid 19 {state} Tracker</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+	  <h1>Covid 19 - {state}</h1>
+	</div>
+</div>
+
+<CovidStat />
+<CovidChart />
