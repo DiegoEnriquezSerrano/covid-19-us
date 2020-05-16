b5efcfc689a79bd139a38a86506261cadaf72904
diego enriquez-serrano <diego@provi.com>
2020-05-16 08:19:16 -0500

    set up page titles

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,13 @@
 <svelte:head>
-	<title>About</title>
+	<title>About Covid-19 Tracker US</title>
 </svelte:head>
+
+  <div class="section header">
+    <div class="container">
+		  <h1>About</h1>
+		  <p>
+			  The data for this site came from <a href="https://covidtracking.com">The Covid-19 Tracker</a>.
+        You can learn more about the project by going to their site.
+			</p>
+		</div><!--container-->
+	</div><!--section header-->
